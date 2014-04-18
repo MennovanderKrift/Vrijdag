c13892e1d5dd8ee53a0a7387bc8c45b783840376
Danneke12 <actionmandanny@hotmial.com>
2014-04-18 16:31:29 +0200

    Proto: Livestream+Index
    Grote aanpassing gemaakt, al bezig geweest met de livestream ik heb
    alles klaarstaan op me laptop :)

---
+++ prototype/Livestream.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vrijdag's super gave en ultra deluxe wedstrijd statistieken!</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+</head>
+
+<body>
+<div id="container">
+    <nav>
+        <a href ="index.php"><img src="assets/img/vrijdag-logo.png" alt="Vrijdag logo" /></a>
+        <ul>
+            <li><a href="livestream.php">Livestream</a></li>
+            <li><a href="#">Laatste Nieuws</a></li>
+            <li><a href="#">Resultaten</a></li>
+            <li><a href="#">Topscorders</a></li>
+            <li><a href="#">Register</a></li>
+            <!--<li><a href="#">Login</a></li>-->
+            <li><a href="#">Score</a></li>
+            <li><a href="#">APO</a> </li>
+        </ul>
+        <div class="clear"></div>
+        <hr>
+    </nav>
+
+    <div id="livestream">
+        <iframe width="640" height="437" src="http://www.ustream.tv/embed/17825957?ub=0034FF&amp;lc=0034FF&amp;oc=0034FF&amp;uc=0034FF&amp;v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;">    </iframe>
+        <iframe width="240" height="437" src="http://www.ustream.tv/socialstream/17825957?siteMode=1?activeTab=socialStream&hideVideoTab=1&colorScheme=light&v=6" frameborder="0" style="border: 0px none transparent"></iframe>
+    </div>
+    <div class="clear">
+        <span class="copyright">Copyright &copy; Unicorn B.V.</span>
+    </div>
+</div>
+
+</body>
+
+</html>
---
+++ prototype/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vrijdag's super gave en ultra deluxe wedstrijd statistieken!</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+</head>
+
+<body>
+<div id="container">
+    <nav>
+        <a href ="index.php"><img src="assets/img/vrijdag-logo.png" alt="Vrijdag logo" /></a>
+        <ul>
+            <li><a href="livestream.php">Livestream</a></li>
+            <li><a href="#">Laatste Nieuws</a></li>
+            <li><a href="#">Resultaten</a></li>
+            <li><a href="#">Topscorders</a></li>
+            <li><a href="#">Register</a></li>
+            <!--<li><a href="#">Login</a></li>-->
+            <li><a href="#">Score</a></li>
+            <li><a href="#">APO</a> </li>
+        </ul>
+        <div class="clear"></div>
+        <hr>
+    </nav>
+
+    <div id="content">
+        <h1> Pagina Naam </h1>
+        <hr>
+        <p> Pagina Content zoals paragraven, stats en afbeeldingen enzo </p>
+        <p> Dit gedeelte zou b.v. ook kunnen worden opgevuld met de laatste nieuwtjes.
+            Blessures van spelers, wie de laatste paar wedstrijden hebben gewonnen etc.</p>
+        <img src="assets/img/radeloos-college.png" />
+        <img src="assets/img/angry-unicorn.png" />
+    </div>
+    <div class="clear">
+        <span class="copyright">Copyright &copy; Unicorn B.V.</span>
+    </div>
+</div>
+
+</body>
+
+</html>
