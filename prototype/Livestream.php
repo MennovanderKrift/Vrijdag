a9c8534c344513e36da1df1eb0e9eb44a56d1966
Danneke12 <actionmandanny@hotmial.com>
2014-04-18 17:06:01 +0200

    Twitter button :)

--- prototype/Livestream.php
+++ prototype/Livestream.php
@@ -3,6 +3,7 @@
 <head>
     <title>Vrijdag's super gave en ultra deluxe wedstrijd statistieken!</title>
     <link rel="stylesheet" href="assets/css/style.css" />
+
     <link rel="icon" type="image/png" href="assets/img/favicon.png" />
 </head>
 
@@ -27,6 +28,7 @@
     <div id="livestream">
         <iframe width="640" height="437" src="http://www.ustream.tv/embed/17825957?ub=0034FF&amp;lc=0034FF&amp;oc=0034FF&amp;uc=0034FF&amp;v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;">    </iframe>
         <iframe width="240" height="437" src="http://www.ustream.tv/socialstream/17825957?siteMode=1?activeTab=socialStream&hideVideoTab=1&colorScheme=light&v=6" frameborder="0" style="border: 0px none transparent"></iframe>
+        <iframe id="twitter-widget-0" scrolling="no" frameborder="0" allowtransparency="true" src="http://platform.twitter.com/widgets/tweet_button.1397165098.html#_=1397831818954&amp;count=horizontal&amp;counturl=http%3A%2F%2Fwww.ustream.tv%2Fchannel%2Fvrijdagshow&amp;id=twitter-widget-0&amp;lang=en&amp;original_referer=http%3A%2F%2Fwww.ustream.tv%2Fchannel%2Fvrijdagshow&amp;size=m&amp;text=VrijdagShow&amp;url=http%3A%2F%2Fustre.am%2F1cNlr" class="twitter-share-button twitter-tweet-button twitter-count-horizontal" title="Twitter Tweet Button" data-twttr-rendered="true" style="width: 110px; height: 20px; display: inline-block;"></iframe>
     </div>
     <div class="clear">
         <span class="copyright">Copyright &copy; Unicorn B.V.</span>
