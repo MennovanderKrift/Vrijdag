ffd07f08a71912aa2888c086d8851da2efdd9625
Menno van der Krift (Laptop) <menno.vd.gmail.com>
2014-06-06 15:36:32 +0200

    Optimalisatie

---
+++ optimalisatie/med_app/includes/navigation.php
@@ -0,0 +1,8 @@
+<header>
+	<ul>
+		<li><a href="">Wedstrijden</a></li>
+		<li><a href="">Reset data</a></li>
+		<li><a href="">Addscore</a></li>
+		<li><a href="">Livestream</a></li>
+	</ul>
+</header>
