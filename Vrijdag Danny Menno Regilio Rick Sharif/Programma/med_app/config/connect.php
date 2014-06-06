f72843745fa3caa8aa3f09fd4d05423e6c5fa1aa
regilio dielemans <regilio_dielemans@hotmail.com>
2014-06-06 16:00:22 +0200

    inleveren

---
+++ Vrijdag Danny Menno Regilio Rick Sharif/Programma/med_app/config/connect.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+try {
+$con = new PDO('mysql:host=localhost;dbname=fifadev', 'root', '');
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+echo 'error: ' . $e->getMessage();
+*/
+
+$con = mysqli_connect('localhost', 'root', '', 'fifadev') or die('Error :' . mysqli_error($con));
+
+
+
+
+
+
+?>
